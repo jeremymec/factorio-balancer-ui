ec9a4168d8bd8b5d4eebf3253b4358264bf2c7e9
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-05-19 00:54:20 +1200

    Consume factorio-blueprint-renderer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React, { useEffect, useRef, useCallback } from 'react'
+import { MyComponent } from 'factorio-blueprint-renderer'
 
 interface BalancerParameters {
   inputs: number,
@@ -7,18 +8,22 @@ interface BalancerParameters {
   splitters: number
 }
 
-
 export default function Home() {
   const workerRef = useRef<Worker>()
 
   useEffect(() => {
     workerRef.current = new Worker(new URL("../worker.ts", import.meta.url))
     workerRef.current.onmessage = (evt) =>
-    alert(`WebWorker Response => ${evt.data}`)
-  })
+      alert(`WebWorker Response => ${evt.data}`)
+    workerRef.current.onerror = function (event) {
+      throw new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
+    }
+  }
+  )
 
   const handleWork = useCallback(async () => {
-    workerRef.current.postMessage({inputs: 1, outputs: 1, splitters: 1})
+    //debugger
+    workerRef.current.postMessage({ inputs: 1, outputs: 1, splitters: 1 })
   }, [])
 
   return (
@@ -30,6 +35,64 @@ export default function Home() {
       <div>
         <button onClick={handleWork}>Hello</button>
       </div>
+      <MyComponent blueprint={blueprintJSON} />
     </div>
   )
+}
+
+let blueprintJSON = {
+  "blueprint": {
+      "icons": [
+          {
+              "signal": {
+                  "type": "item",
+                  "name": "transport-belt"
+              },
+              "index": 1
+          }
+      ],
+      "entities": [
+          {
+              "entity_number": 1,
+              "name": "transport-belt",
+              "position": {
+                  "x": 19.5,
+                  "y": 9.5
+              },
+              "direction": 2
+              RIGHT BELT
+          },
+          {
+              "entity_number": 2,
+              "name": "transport-belt",
+              "position": {
+                  "x": 19.5,
+                  "y": 10.5
+              },
+              "direction": 4
+              DOWN BELT
+          },
+          {
+              "entity_number": 3,
+              "name": "transport-belt",
+              "position": {
+                  "x": 18.5,
+                  "y": 10.5
+              }
+              UP BELT
+          },
+          {
+              "entity_number": 4,
+              "name": "transport-belt",
+              "position": {
+                  "x": 20.5,
+                  "y": 10.5
+              },
+              "direction": 2
+              RIGHT BELT
+          }
+      ],
+      "item": "blueprint",
+      "version": 281479273840640
+  }
 }
--- src/worker.ts
+++ src/worker.ts
@@ -3,8 +3,9 @@ import generate from 'factorio-balancer';
 const ctx: Worker = self as any;
 
 ctx.addEventListener('message', (event) => {
-  const result = generate(1,2,1)
-  debugger
-  console.log("Hello")
-  ctx.postMessage('Better!')
+  console.log("Generating")
+
+  const result = generate(3,3,4)
+  console.log("Done")
+  ctx.postMessage(result)
 })

a0db367462fec3e88a87f6f5ebdd9106ce568996
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-06-13 17:49:57 +1200

    Broken worker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,72 +3,98 @@ import React, { useEffect, useRef, useCallback, useState } from 'react'
 import { RenderedBlueprint } from 'factorio-blueprint-renderer'
 import { Button, TextField, Container, Box, Grid, Input } from '@material-ui/core'
 
-interface BalancerParameters {
-  inputs: number,
-  outputs: number,
-  splitters: number
-}
 
 export default function Home() {
-  const workerRef = useRef<Worker>()
+    // const workerRef = useRef<Worker[]>([]);
 
-  const [blueprintData, setBlueprintData] = useState(undefined)
-  const [blueprintLoading, setBlueprintLoading] = useState(true)
+    const [workers, setWorkers] = useState([])
 
-  const [inputs, setInputs] = useState("")
-  const [outputs, setOutputs] = useState("")
+    const [blueprintData, setBlueprintData] = useState(undefined);
+    const [blueprintLoading, setBlueprintLoading] = useState(true);
+    
+    const [inputs, setInputs] = useState("");
+    const [outputs, setOutputs] = useState("");
+    const [splitters, setSplitters] = useState("");
 
-  useEffect(() => {
-    workerRef.current = new Worker(new URL("../worker.ts", import.meta.url))
-    workerRef.current.onmessage = (evt) => {
-      console.log("Sending blueprint")
-      setBlueprintData(evt.data)
-      setBlueprintLoading(false)
+    const workerCount = 10;
+
+    const workerKeepAlive = useRef(false);
+
+    useEffect(() => {
+        for (let i = 0; i < workerCount; i++) {
+            const worker = new Worker(new URL("../worker.ts", import.meta.url));
+            setWorkers([worker])
+
+            worker.onmessage = (evt) => {
+                setBlueprintData(evt.data);
+                setBlueprintLoading(false);
+                workerKeepAlive.current = false;
+            }
+
+            worker.onerror = (evt) => {
+                throw new Error(evt.message + " (" + evt.filename + ":" + evt.lineno + ")");
+            }
+        }
+        console.log("Workers" + workers)
+    }, [])
+
+    useEffect(() => {
+
+        const keepalive = () => {
+            if (workerKeepAlive.current) {
+                for (let i = 0; i < workerCount; i++) {
+                    console.log("Workers in keep alive" + workers)
+                    const worker = workers[i];
+                    worker.postMessage({
+                        payload: {
+                            inputs: Number(inputs),
+                            outputs: Number(outputs),
+                            splitters: Number(1 + i)
+                        }
+                    });
+                }
+            }
+
+            setTimeout(keepalive, 500);
+        };
+
+        setTimeout(keepalive, 500);
+    }, [workerKeepAlive.current]);
+
+    function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        workerKeepAlive.current = true
     }
-    workerRef.current.onerror = function (event) {
-      throw new Error(event.message + " (" + event.filename + ":" + event.lineno + ")");
-    }
-  }
-  )
 
-  function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-  }
-
-  const handleWork = useCallback(async () => {
-    workerRef.current.postMessage({ inputs: 1, outputs: 2, splitters: 1 })
-  }, [])
-
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Container maxWidth="sm">
-          <Grid container spacing={3}>
-          <form onSubmit={handleWork}>
-            <Grid container item>
-              <Box>
-                  <TextField id="standard-basic" label="Inputs" value={inputs} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setInputs(e.target.value)}/>
-                  <TextField id="standard-basic" label="Outputs" value={outputs} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOutputs(e.target.value)}/>
-              </Box>
-            </Grid>
-            <Grid container item>
-              <Box>
-                <Button variant="contained" color="primary" type="submit" value="Submit">Generate</Button>
-                <Button variant="contained" color="primary" onClick={handleWork}>Testy</Button>
-              </Box>
-            </Grid>
-            <Grid item style={{flex: 1}}>
-              <RenderedBlueprint blueprintData={blueprintData} isLoading={blueprintLoading} />
-            </Grid>
-            </form>
-          </Grid>
-        </Container>
-      </body>
-    </div>
-  )
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <Container maxWidth="sm">
+                    <Grid container spacing={3}>
+                        <form onSubmit={handleFormSubmit}>
+                            <Grid container item>
+                                <Box>
+                                    <TextField id="standard-basic" label="Inputs" value={inputs} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setInputs(e.target.value)} />
+                                    <TextField id="standard-basic" label="Outputs" value={outputs} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setOutputs(e.target.value)} />
+                                    <TextField id="standard-basic" label="Splitters" value={splitters} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setSplitters(e.target.value)} />
+                                </Box>
+                            </Grid>
+                            <Grid container item>
+                                <Box>
+                                    <Button variant="contained" color="primary" type="submit" value="Submit">Generate</Button>
+                                </Box>
+                            </Grid>
+                            <Grid item style={{ flex: 1 }}>
+                                <RenderedBlueprint blueprintData={blueprintData} isLoading={blueprintLoading} />
+                            </Grid>
+                        </form>
+                    </Grid>
+                </Container>
+            </body>
+        </div>
+    );
 }
--- src/worker.ts
+++ src/worker.ts
@@ -2,10 +2,22 @@ import generate from 'factorio-balancer';
 
 const ctx: Worker = self as any;
 
-ctx.addEventListener('message', (event) => {
-  console.log("Generating")
+interface Payload {
+  inputs: number,
+  outputs: number,
+  splitters: number
+}
 
-  const result = generate(3,3,4)
-  console.log("Done")
-  ctx.postMessage(result)
+ctx.addEventListener('message', (event) => {
+  console.log("alive")
+  const { inputs, outputs, splitters } = event.data.payload as Payload;
+
+  console.log("Inputs: " + inputs)
+  console.log("Outputs: " + outputs)
+  console.log("Splitters: " + splitters)
+
+  const result = generate(inputs, outputs, splitters, [1, 1], { iterationLimit: 1000})
+  if (!!result) {
+    ctx.postMessage(result);
+  }
 })

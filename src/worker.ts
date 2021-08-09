c32259897a12ad01b8713a69fee0854ad5d899d2
Jeremy Mechen <jeremymechen1997@gmail.com>
2021-08-10 01:31:59 +1200

    Add error handling to worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,23 +1,41 @@
-import { generateNodes } from 'factorio-balancer'; 
+import { generateNodes, FactorioNode } from 'factorio-balancer'; 
 
 const ctx: Worker = self as any;
 
 export interface Payload {
   inputs: number,
   outputs: number,
+  splitters: number
+}
+
+export interface Result {
+  isError: boolean,
+  nodes: FactorioNode[]
 }
 
 ctx.addEventListener('message', (event) => {
 
   const inputs = event.data.inputs
   const outputs = event.data.outputs
+  const splitters = event.data.splitters
 
   console.log("Inputs: " + inputs)
   console.log("Outputs: " + outputs)
+  console.log("Splitters: " + splitters)
 
-  const result = generateNodes(2, 3, 6)
-  console.log(result)
-  if (!!result) {
-    ctx.postMessage(result);
+  try {
+    const nodes = generateNodes(inputs, outputs, splitters)
+    if (!!nodes) {
+      ctx.postMessage({
+        isError: false,
+        nodes: nodes
+      });
+    }
+  } catch {
+    ctx.postMessage({
+      isError: true,
+      nodes: []
+    })
   }
+
 })
